080784d3ce15db78c5f5bb5186d4bbd9ce78341e
Quint0n53 <96028968+Quint0n53@users.noreply.github.com>
2022-03-02 11:44:01 -0500

    Update TextGame.cpp

--- TextGame.cpp
+++ TextGame.cpp
@@ -13,18 +13,25 @@ vector <char> letters;
 int attempts = 3;
 int level;
 char gotodoor;
+char tosave;
 int hints = 0;
 fstream gamefile;//most important for external files
+string filename;
 
 void twohundred();
 void fourhundred();
 void gameover();
 void onehundred();
+void threehundred();
 void save() {
+	gamefile.close();
+	ofstream gamefile(filename);
 	for (int i = 0; i < 5; i++) {
 		gamefile << progress[i] << endl;
 	}
+	cout << "Your file name is " << filename;
 	gamefile.close();
+	exit(0);
 }
 void clear() {//explain this later when you know whats happening
 	COORD topLeft = { 0, 0 };//identify top left
@@ -38,7 +45,7 @@ void clear() {//explain this later when you know whats happening
 	);
 	SetConsoleCursorPosition(console, topLeft);//reset to top corner
 }
-void hint(){
+void hint() {
 	hints++;
 	cout << "You have chosen a great room. In this room the staff member passes you a note.\nAt the very top in big bold letters it says HINT\nYou are beyond grateful\n";
 	switch (hints) {
@@ -51,7 +58,7 @@ void hint(){
 		cout << "To yourself you think these hints are becoming much more helpful.\nAt what point will you be given the solution..\n";
 		break;
 	case 3:
-		cout << "This is your final hint. After this you have lost the game\nThe hint appears to no longer be a hint but rather the answers 3, puzzle and LLLRUDR\n";
+		cout << "This is your final hint. After this you have lost the game\nThe hint appears to no longer be a hint but rather the answers 3, puzzle and RRRRUULLLLLULLDDDDDD\n";
 		cout << "You pray to yourself that this is all you will need to escape finally\n";
 		break;
 	case 4:
@@ -71,7 +78,7 @@ void hint(){
 		fourhundred();
 	}
 }
-void gameover() {
+void gameover() {//
 	cout << "Unfortunately you have lost the game. You are stuck in the school forever...\n";
 	save();
 	exit(0);
@@ -81,7 +88,7 @@ void piovesan() {//ask for a french sentence
 		cout << "You have found Mme Piovesan in room 212\nYou ask her for help as you just want to leave\nShe responds in French with:\nBonjour, Pouvez vous traduire le mot casse-tete pour moi\n";
 		cout << "Please enter what you believe the translation of casse-tete is in english:";
 		cin >> french;
-		while (french != "puzzle") { 
+		while (french != "puzzle") {
 			cout << "Essayer encore sil-vous-plait:";
 			cin >> french;
 		}
@@ -121,12 +128,56 @@ void kempe() {//solving math equation
 	else {
 		cout << "You have returned to room 318. The door is locked since you have already come here and Mr Kempe cannot be found\nBetter move onto a different room";
 	}
-
+	threehundred();
 }
-void robot() {//drive to then shoot?
-	letters.push_back('R');
-	letters.push_back('D');
-	letters.push_back('A');
+void robot() {//drive to then shoot? //LLLRUDR
+	if (progress[3] == 0) {
+		string robot;
+		cout << "Welcome to the innovation space; the robotics club home.\nYou see Mr Varsava is trying to help the robot leave the field\n";
+		cout << "Please tell him which direction to move the robot all in one large sequence\n(R is right, L is Left, U is Up, D is Down)\n";
+		cout << "|_ _ _ _ _ _ _ _ _ _ _ _|\n";
+		cout << "|_ _ _ _ _ _ _ _ _ _ _ _|\n";
+		cout << "|_ _ _ _ _ _ _ _ _ _ _ _|\n";
+		cout << "|_ _ _ _ _ _ _ _ _ _ _ _|\n";
+		cout << "|_ _ X X X X X X _ _ _ _|\n";
+		cout << "|_ _ X _ _ _ X X X X X _|\n";
+		cout << "|_ _ X _ X _ _ _ _ _ X _|\n";
+		cout << "|_ _ X _ X X X X X _ X _|\n";
+		cout << "|_ _ X _ X R _ _ _ _ X _|\n";
+		cout << "|_ _ X _ X X X X X X X _|\n";
+		cout << "|_ _ X _ X _ _ _ _ _ _ _|\n";
+		cout << "|_ _ X _ X _ _ _ _ _ _ _|\n";
+		cin >> robot;
+		while (robot != "RRRRUULLLLLULLDDDDDD") {
+			clear();
+			cout << "Try again, the robot did not exit the field ";
+			cout << "Please tell him which direction to move the robot all in one large sequence\n(R is right, L is Left, U is Up, D is Down)\n";
+			cout << "|_ _ _ _ _ _ _ _ _ _ _ _|\n";
+			cout << "|_ _ _ _ _ _ _ _ _ _ _ _|\n";
+			cout << "|_ _ _ _ _ _ _ _ _ _ _ _|\n";
+			cout << "|_ _ _ _ _ _ _ _ _ _ _ _|\n";
+			cout << "|_ _ X X X X X X _ _ _ _|\n";
+			cout << "|_ _ X _ _ _ X X X X X _|\n";
+			cout << "|_ _ X _ X _ _ _ _ _ X _|\n";
+			cout << "|_ _ X _ X X X X X _ X _|\n";
+			cout << "|_ _ X _ X R _ _ _ _ X _|\n";
+			cout << "|_ _ X _ X X X X X X X _|\n";
+			cout << "|_ _ X _ X _ _ _ _ _ _ _|\n";
+			cout << "|_ _ X _ X _ _ _ _ _ _ _|\n";
+			cin >> robot;
+			cin >> robot;
+		}
+		cout << "Congratulations, the robot has successfuly exited the field.\nYou see the letters R D A on the side of the robot\n";
+		cout << "As you leave Mr Kempe jumps into his ceiling beneath the painted tiles...never to be seen again\n";
+		letters.push_back('R');
+		letters.push_back('D');
+		letters.push_back('A');
+		progress[3] = 1;
+	}
+	else {
+		cout << "You have already saved the robot in the Innovation space\nBetter move onto a different room";
+	}
+	fourhundred();
 }
 void door() {//ask for secret word
 	cout << "You have arrived at the front door.\nSeemingly the only escape point of the school.\nOn the door you see a padlock requiring 10 letters\n";
@@ -192,26 +243,44 @@ void twohundred() {
 		twohundred();
 		break;
 	case 236: //Hrycak
-		cout << "You have entered room 236\n";
+		cout << "You have entered room 236\nInside you find Mr Hrycak making an espresso he offers you some help with the puzzle\n";
 		hint();
 		break;
 	case 235: //Reitzel
-		cout << "You have entered room 235\n";
+		cout << "You have entered room 235\nYou find Mr Reitzel practing his french for the DELF, he informs you that there is nothing in his room to help you";
+		twohundred();
 		break;
 	case 234: //Hillyer
-		cout << "You have entered room 234\n";
+		cout << "You have entered room 234\nInside you see the ghost of Ms Hillyer who no longer works at our school\nYou immedietely run out of the door\n";
+		cout << "You are faced with three options, stay on the 200s, go up the stairs to the 300s or head outside to the MTO building\n";
+		cout << "For 200s enter a 2, for 300s enter a 3 and for the MTO enter a 4";
+		cin >> level;
+		if (level == 2) {
+			twohundred();
+		}
+		else if (level == 3) {
+			progress[0] = 320;
+			threehundred();
+		}
+		else if (level == 4) {
+			progress[0] = 401;
+			fourhundred();
+		}
+		else {
+			twohundred();
+		}
 		break;
 	case 228: //DRAMA
-		cout << "You have entered room 228\n";
-		break;
-	case 211: //Neilly
-		cout << "You have entered room 211\n";
+		cout << "You have entered room 228\nThis is where the magic of the school happens\nYou see Mrs Ward dancing and opt to not bother her\nProceed to a differnet room";
+		twohundred();
 		break;
 	case 210: //Mr Fitz
-		cout << "You have entered room 210\n";
+		cout << "You have entered room 210\nMr Fitzsimmons is teaching a lesson about his favorite band and how he keeps his beard so lucious\nYou accept his tips and move onto the next room\n";
+		twohundred();
 		break;
 	case 209://Hogan
-		cout << "You have entered room 209\n";
+		cout << "You have entered room 209\nMr Hogan welcomes you in, he wishes that he had more for you\nUnfortunately he is very busy with other students\Proceed to another room\n";
+		twohundred();
 		break;
 	default://outside
 		cout << "no room";
@@ -227,28 +296,72 @@ void threehundred() {
 	case 318:
 		kempe();
 		break;
-	case 320://Empty
+	case 339://Empty
+		cout << "You have entered the save room, would you like to save your progress Y or N";
+		cin >> tosave;
+		if (tosave == 'Y' || tosave == 'y') {
+			save();
+		}
+		else {
+			cout << "You shall now move to another room,\n";
+			threehundred();
+		}
 		break;
 	case 319://Walter
+		cout << "Mr Walter in in the back of his room playing Clash of Clans\nHe looks very busy better not ask him for help\n";
+		threehundred();
 		break;
 	case 315://Ward
+		cout << "As you peek into room 315, you barely see the top of someones head\nUopn operning the door you see Mr Ward teching titrations\nYou didnt understand these when you took his course\nBetter leave while you can\n";
+		threehundred();
 		break;
 	case 313://Takken
-		break;
-	case 326://Mcgee
+		cout << "The very front of the school, Ms Takkens room\nInside you see her playing a game of trivia with herself\nShe asks you one question, you did not get it right but decide to move on\n";
+		threehundred();
 		break;
 	case 327: //Hannusch
+		cout << "Possibly the longest teaching teacher currently at SMHS\nIn room 327 you find Mr Hannusch giving a philosophy lesson\nIts inspiring but cannot help you\nBetter keep moving\n";
+		threehundred();
 		break;
 	case 328://Ms Hannusch
+		cout << "Husband and wife are next to each other\nYou ask Ms Hannusch for help, She offers you a cup of tea but cannot give you any assistance\n";
+		threehundred();
 		break;
 	case 329://Kim
+		cout << "Walking down the hallway you hear grunting coming from one room\nUpon further nvestigation its Master Kim practing his martial arts\nHe challenges you to a duel\nIn hopes of preserving your life you decline the fight\n";
+		threehundred();
 		break;
 	case 301://Snider
+		cout << "You walk into the secret room, from the door you spot Ms Snider with her cute baby\n you spend the next few minutes playing and giving her a break from the crying baby\nAfter 5 minutes you chose to leave and try and find some help\n";
+		threehundred();
 		break;
 	case 302://Annetta
+		cout << "You open the door to 302, it is an absolute Sauna, inside you find Mr Anetta passing out and dripping sweat\nYou find him some water and offer to take him with you\nHe does'nt want to be a bother so he stays there\nBetter leave before you become another victim\n";
+		threehundred();
+		break;
+	case 320:
+		cout << "You have entered room 320\nInside you see the ghost of Ms Hillyer who no longer works at our school\nYou immedietely run out of the door\n";
+		cout << "You are faced with three options, stay on the 300s, go down the stairs to the 200s or head outside to the MTO building\n";
+		cout << "For 200s enter a 2, for 300s enter a 3 and for the MTO enter a 4";
+		cin >> level;
+		if (level == 2) {
+			progress[0] = 234;
+			twohundred();
+		}
+		else if (level == 3) {
+			threehundred();
+		}
+		else if (level == 4) {
+			progress[0] = 401;
+			fourhundred();
+		}
+		else {
+			threehundred();
+		}
 		break;
 	default://outside
 		cout << "no room";
+		threehundred();
 	}
 }
 void onehundred() {
@@ -271,7 +384,7 @@ void onehundred() {
 			cout << "You chose to remain on the 100 floor.\n";
 			onehundred();
 		}
-		else if(level == 2){
+		else if (level == 2) {
 			clear();
 			cout << "upstairs you go, good luck!";
 			progress[0] = 209;
@@ -309,10 +422,18 @@ void onehundred() {
 	}
 }
 void fourhundred() {
+	cout << "You are currently in room " << progress[0] << "\nYou may advance to any room on this floor(Coop(401), Innovation(402), Shop(400))";
+	cout << "\nWhere would you like to go:";
+	cin >> progress[0];
+	clear();
 	switch (progress[0]) {
 	case 400://Shop
+		cout << "Unfortunately you did not wear eye protection while entering the shop. Your game is now over.";
+		gameover();
 		break;
 	case 401://Co-op
+		cout << "In the coop room you dont see anyone. Just an empty space that looks unsued.\nProceed to a different room";
+		fourhundred();
 		break;
 	case 402://robot
 		robot();
@@ -325,7 +446,7 @@ void fourhundred() {
 int main() {
 	string theLine;//using to get data from file
 	char oldfile;
-	string filename;
+	
 	bool open = false;
 	cout << "Welcome to the Role Playing Game\nYou are a grade 9 student of Saint Mary's who has been locked inside the building on your very first day.\nYou're knowledge of the rooms and teachers will be put to the test\n";
 	do {
@@ -358,6 +479,7 @@ int main() {
 	clear();//clear out save stuff
 	cout << "To win the game you must visit 3 different areas of the school and complete a task at each location\nThe game will begin shortly...\n";
 	cout << "You begin in the computer lab where I assume you are playing this game.\nYou will move from room to room as well as using the stairs and portals to travel from point to point\nGOOD LUCK!\n";
+	cout << "To save your progress head to room 320 at any point during the game\n";
 	progress[0] = 127;
 	onehundred();
 	return 0;
